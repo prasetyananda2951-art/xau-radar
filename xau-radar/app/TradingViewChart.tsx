ed41bf7c068e58556101e73317ba91a3e4adc9eb
prasetyananda2951-art <prasetyananda2951@gmail.com>
2026-04-15 16:42:41 +0700

    Update TradingViewChart.tsx

--- xau-radar/app/TradingViewChart.tsx
+++ xau-radar/app/TradingViewChart.tsx
@@ -1,33 +1,62 @@
-"use client";
-import { useEffect, useRef } from "react";
+'use client';
 
-export default function TradingViewChart() {
-  const container = useRef<HTMLDivElement>(null);
+import React, { useEffect, useRef } from 'react';
+
+declare global {
+  interface Window {
+    TradingView: any;
+  }
+}
+
+const TradingViewChart = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!container.current) return;
-    const script = document.createElement("script");
-    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-kline.js";
-    script.type = "text/javascript";
+    // Membuat ID unik agar tidak bentrok
+    const containerId = 'tradingview_xau_radar';
+    if (containerRef.current) {
+      containerRef.current.id = containerId;
+    }
+
+    const script = document.createElement('script');
+    script.src = 'https://s3.tradingview.com/tv.js';
     script.async = true;
-    script.innerHTML = JSON.stringify({
-      container_id: "tradingview_chart",
-      symbol: "OANDA:XAUUSD",
-      interval: "15",
-      timezone: "Etc/UTC",
-      theme: "dark",
-      style: "1",
-      locale: "en",
-      enable_publishing: false,
-      allow_symbol_change: true,
-      container_id: "tradingview_chart",
-    });
-    container.current.appendChild(script);
+    script.onload = () => {
+      if (window.TradingView && document.getElementById(containerId)) {
+        new window.TradingView.widget({
+          "autosize": true,
+          "symbol": "OANDA:XAUUSD",
+          "interval": "H1",
+          "timezone": "Etc/UTC",
+          "theme": "dark",
+          "style": "1",
+          "locale": "en",
+          "toolbar_bg": "#f1f3f6",
+          "enable_publishing": false,
+          "hide_side_toolbar": false,
+          "allow_symbol_change": true,
+          "container_id": containerId,
+        });
+      }
+    };
+    document.head.appendChild(script);
+
+    return () => {
+      // Membersihkan script saat komponen ditutup
+      if (script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    };
   }, []);
 
   return (
-    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
-      <div id="tradingview_chart" style={{ height: "100%", width: "100%" }} />
+    <div className="tradingview-widget-container">
+      <div 
+        ref={containerRef} 
+        style={{ height: '600px', width: '100%' }} 
+      />
     </div>
   );
-}
+};
+
+export default TradingViewChart;
